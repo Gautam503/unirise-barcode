a7cbf9f5399bd80fce24a29de6b9722dd1cc48dc
Gautam503 <121816275+Gautam503@users.noreply.github.com>
2026-04-05 18:02:49 +0530

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -287,4 +287,5 @@ UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
