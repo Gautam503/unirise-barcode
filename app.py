7ff70d6009ce89d75eb54b9add6ea5467d9306dd
Gautam503 <121816275+Gautam503@users.noreply.github.com>
2026-04-05 17:28:21 +0530

    Change app.run to specify port 5000

--- app.py
+++ app.py
@@ -287,4 +287,4 @@ UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
