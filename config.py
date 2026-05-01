db3bf899da2245259d04cdce1e635311a149d569
Gautam <gautam2002jha@gmail.com>
2026-05-01 08:58:12 +0530

    version 3.1

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "gpljha.medi@gmail.com"
-    MAIL_PASSWORD = "exytrcneoxgwddoe"
-    MAIL_DEFAULT_SENDER = "gpljha.medi@gmail.com"
+
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
 
